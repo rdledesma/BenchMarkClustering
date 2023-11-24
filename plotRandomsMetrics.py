9bde168a13fc8d1bd1cbda26707cef3af156a567
rdledesma <rdledesma1995@gmail.com>
2023-11-24 09:35:53 -0300

    add Randoms Metrics

--- plotRandomsMetrics.py
+++ plotRandomsMetrics.py
@@ -14,28 +14,44 @@ dmae = pd.read_csv('randomMetrics/mae.csv')
 dmbe = pd.read_csv('randomMetrics/mbe.csv')
 
 
+
+dmbe.columns = ['2', '3', '4', '5', '6', '7', '8', '9',
+       '10', '11', '12']
+
+
+
+
+drmsd.columns = ['2', '3', '4', '5', '6', '7', '8', '9',
+       '10', '11', '12']
+
+
+
+dmae.columns = ['2', '3', '4', '5', '6', '7', '8', '9',
+       '10', '11', '12']
+
+
+
 # Calcular la media y el desvío estándar para cada columna
-means = drmsd.mean()
-stds = drmsd.std()
+# means = drmsd.mean()
+# stds = drmsd.std()
 
-# Crear el gráfico
-plt.figure(figsize=(10, 6))
+# # Crear el gráfico
+# plt.figure(figsize=(10, 6))
 
-# Para cada columna, trazar la media y sombrear el rango de un desvío estándar
-for column in means.index:
-    plt.errorbar(x=column, y=means[column], yerr=stds[column], fmt='o', label=column)
+# # Para cada columna, trazar la media y sombrear el rango de un desvío estándar
+# for column in means.index:
+#     plt.errorbar(x=column, y=means[column], yerr=stds[column], fmt='o', label=column)
 
-# Añadir etiquetas y leyenda
-plt.xlabel('Columnas')
-plt.ylabel('Valor Medio')
-plt.title('Valores Medios y Rango de Desvío Estándar')
-plt.legend()
-plt.grid(True)
-plt.show()
+# # Añadir etiquetas y leyenda
+# plt.xlabel('Columnas')
+# plt.ylabel('Valor Medio')
+# plt.title('Valores Medios y Rango de Desvío Estándar')
+# plt.legend()
+# plt.grid(True)
+# plt.show()
 
     
-import pandas as pd
-import matplotlib.pyplot as plt
+
 
 # Supongamos que tienes tres DataFrames llamados df1, df2 y df3 con las columnas met_2, met_3, met_4, ...
 
@@ -46,6 +62,7 @@ import matplotlib.pyplot as plt
 means = dmbe.mean()
 stds = dmbe.std()
 
+
 # Crear el gráfico
 plt.figure(figsize=(10, 6))
 
@@ -55,17 +72,81 @@ for column in means.index:
 
 # Unir los valores medios con una línea punteada
 # Rellenar el área entre los máximos y mínimos con color azul
-plt.fill_between(means.index, means - stds, means + stds, color='blue', alpha=0.2)
+plt.fill_between(means.index, means - stds, means + stds, color='blue', alpha=0.1)
+plt.plot(means.index, means - stds,  color='gray', linestyle='--', alpha=0.3, label="Min STD")
+plt.plot(means.index, means + stds,  color='gray', linestyle='dotted', alpha=0.3, label="Max STD")
 
-plt.plot(means.index, means.values, linestyle='--', marker='o', color='black', label='Media')
+plt.plot(means.index, means.values, linestyle='--', marker='o', color='black', label='Mean value')
 
 # Añadir etiquetas y leyenda
-plt.xlabel('Columnas')
-plt.ylabel('Valor Medio')
-plt.title('Valores Medios y Rango de Desvío Estándar')
-plt.legend()
+plt.xlabel('Clusters cuantity')
+plt.ylabel('MBE %')
+#plt.title('Valores Medios y Rango de Desvío Estándar')
+plt.legend(loc='upper left')
 # plt.grid(True)
 plt.show()
+
+
+
+
+# Calcular la media y el desvío estándar para cada columna
+means = dmae.mean()
+stds = dmae.std()
+
+
+# Crear el gráfico
+plt.figure(figsize=(10, 6))
+
+# Para cada columna, trazar la media y sombrear el rango de un desvío estándar
+for column in means.index:
+    plt.errorbar(x=column, y=means[column], yerr=stds[column], fmt='o', label=column)
+
+# Unir los valores medios con una línea punteada
+# Rellenar el área entre los máximos y mínimos con color azul
+plt.fill_between(means.index, means - stds, means + stds, color='blue', alpha=0.1)
+plt.plot(means.index, means - stds,  color='gray', linestyle='--', alpha=0.3, label="Min STD")
+plt.plot(means.index, means + stds,  color='gray', linestyle='dotted', alpha=0.3, label="Max STD")
+
+plt.plot(means.index, means.values, linestyle='--', marker='o', color='black', label='Mean value')
+
+# Añadir etiquetas y leyenda
+plt.xlabel('Clusters cuantity')
+plt.ylabel('MAE %')
+#plt.title('Valores Medios y Rango de Desvío Estándar')
+plt.legend(loc='lower left')
+# plt.grid(True)
+plt.show()
+
+
+# Calcular la media y el desvío estándar para cada columna
+means = drmsd.mean()
+stds = drmsd.std()
+
+
+# Crear el gráfico
+plt.figure(figsize=(10, 6))
+
+# Para cada columna, trazar la media y sombrear el rango de un desvío estándar
+for column in means.index:
+    plt.errorbar(x=column, y=means[column], yerr=stds[column], fmt='o', label=column)
+
+# Unir los valores medios con una línea punteada
+# Rellenar el área entre los máximos y mínimos con color azul
+plt.fill_between(means.index, means - stds, means + stds, color='blue', alpha=0.1)
+plt.plot(means.index, means - stds,  color='gray', linestyle='--', alpha=0.3, label="Min STD")
+plt.plot(means.index, means + stds,  color='gray', linestyle='dotted', alpha=0.3, label="Max STD")
+
+plt.plot(means.index, means.values, linestyle='--', marker='o', color='black', label='Mean value')
+
+# Añadir etiquetas y leyenda
+plt.xlabel('Clusters cuantity')
+plt.ylabel('RMSD %')
+#plt.title('Valores Medios y Rango de Desvío Estándar')
+plt.legend(loc='lower left')
+# plt.grid(True)
+plt.show()
+
+
 
 
 
